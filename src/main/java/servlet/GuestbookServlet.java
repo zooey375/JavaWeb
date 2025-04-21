514b67b5ec021d10a2c6cf612c89c0e3e13c8df6
zooey <yu.ting375@gmail.com>
2025-04-21 16:07:01 +0800

    20250421_0406

--- src/main/java/servlet/GuestbookServlet.java
+++ src/main/java/servlet/GuestbookServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -18,15 +19,20 @@ public class GuestbookServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+		req.getRequestDispatcher("/WEB-INF/guestbook_form.jsp").forward(req, resp);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+		String message = req.getParameter("message"); // 取得表單中的留言內容
+		// 建立  guestbook 物件。
+		Guestbook guestbook = new Guestbook(message);
+		// 加入到 guestbooks 集合中
+		guestbooks.add(guestbook);
+		// 重導到 /WEB-INF/guestbook_result.jsp
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/guestbook_result.jsp");
+		req.setAttribute("message", message); // 本次留言
+		req.setAttribute("guestbooks", guestbooks); // 歷史留言
+		rd.forward(req, resp);
 	}
-	
-	
-	
-	
 }
