21132bedba8cd6802ae2444faeeaf32c5b7e7ad4
zooey375 <yu.ting375@gmail.com>
2025-05-22 10:39:08 +0800

    Update WelcomeServlet.java

--- src/main/java/servlet/WelcomeServlet.java
+++ src/main/java/servlet/WelcomeServlet.java
@@ -7,6 +7,8 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import util.Util;
+
 
 @WebServlet("/welcome")
 public class WelcomeServlet extends HttpServlet {
